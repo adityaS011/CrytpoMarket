e4cc2a9ed63fae7843a136c5bdf4d5552e890796
adityaS011 <adityasinghtech011@gmail.com>
2024-03-07 17:09:12 +0530

    hero

--- src/Pages.jsx
+++ src/Pages.jsx
@@ -1,16 +1,43 @@
-import React from 'react';
+import React,{useState, useEffect} from 'react';
 import Hero from './components/Hero';
 import Sidebar from './components/Sidebar';
+import axios from 'axios';
 
 const Pages = () => {
+  const [cryptoData, setCryptoData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          'https://api.coingecko.com/api/v3/coins/markets',
+          {
+            params: {
+              vs_currency: 'usd',
+              order: 'market_cap_desc',
+              per_page: 5,
+              page: 1,
+              sparkline: false,
+            },
+          }
+        );
+
+        setCryptoData(response.data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
   return (
     <div className=" flex flex-row">
       <div className="w-2/3 pr-4"> {/* 70% width with right padding */}
-        <Hero />
+        <Hero cryptoData={cryptoData} />
         {/* Add other components here for the left side */}
       </div>
       <div className=" w-[420px] h-12 right-0 mr-8 mt-8"> {/* 30% width */}
-        <Sidebar /> {/* Right side fixed sidebar with width 30% */}
+        <Sidebar cryptoData={cryptoData}/> {/* Right side fixed sidebar with width 30% */}
         
       </div>
     </div>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,14 +1,37 @@
 import React from 'react'
 
-const Hero = () => {
+const Hero = ({cryptoData}) => {
+  const topCrypto = cryptoData[0];
+
+  if (!topCrypto) {
+    return null; // Handle case where data is not available
+  }
+
   return (
     <>
     <div className='mt-2'>
-      <span class="text-gray-500 ml-5 ">Cryptocurrencies</span>
-    <span class="mx-1 ">&gt;&gt;</span>
-    <span class="font-[500] text-black-800">Bitcoin</span>
+      <span className="text-gray-500 ml-5 ">Cryptocurrencies</span>
+    <span className="mx-1 ">&gt;&gt;</span>
+    <span className="font-[500] text-black-800">Bitcoin</span>
     </div>
-    <div className=''> </div>
+    <div className='bg-white h-screen mt-4 ml-7 rounded-lg'> 
+    <ul className=' p-4'>
+    <li key={topCrypto.id} className=' flex items-center '>
+        <img
+          src={topCrypto.image} // or topCrypto.image.large
+          alt={topCrypto.name}
+          style={{ width: '40px', marginRight: '8px' }}
+        />
+        <span className='font-semibold text-2xl mr-2 '>{topCrypto.name} 
+        
+        </span>
+        <div className='text-gray-500 font-semibold text-[1.05rem] flex '> 
+          {topCrypto.symbol.toUpperCase()}
+        </div>
+        <div className='ml-7 bg-slate-500 font-medium  text-white text-[17px] py-2 px-2 rounded-lg'>Rank #1</div>
+      </li> 
+    </ul>
+     </div>
     </>
   )
 }
