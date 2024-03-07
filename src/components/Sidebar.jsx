162e15b8eb3e813b88aa130adf4ceb09be91564c
adityaS011 <adityasinghtech011@gmail.com>
2024-03-07 16:50:14 +0530

    sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,13 @@
+
 import React from 'react';
 import koinxSider from '../assets/koinXsider.png';
-const Sidebar = () => {
+
+
+const Sidebar = ({cryptoData}) => {
+
   return (
-    <>
-    <div className='bg-gradient-to-b from-blue-600 via-blue-700 to-blue-600 w-full p-9 flex flex-col items-center text-center justify-center rounded-xl'>
+    <div className='lg:flex flex-col hidden'>
+    <div className='bg-gradient-to-b from-blue-600 via-blue-700 to-blue-600 w-full mt-5 p-9 flex flex-col items-center text-center justify-center rounded-xl'>
       <h1 className='text-white font-semibold text-3xl mb-4'>Get Started with KoinX for FREE</h1>
       <p className='text-gray-300 text-sm mb-6 px-3'>
         With our range of features that you can equip for free,
@@ -17,24 +21,23 @@ const Sidebar = () => {
     <div className='h-80'>
        <h1 className='text-[25px] pt-8 px-8 font-semibold '>Trending Coins {'(24h)'}</h1>
        <ul className='p-4 px-8'>
-      <li className='py-2 flex items-center'>
-        <span className='text-lg font-medium mr-2'>Ethereum(ETH)</span>
-        <div className='triangle'></div>
-        <span className='percentage text-lg font-[600] ml-auto text-green-500'>8.21%</span>
-      </li>
-      <li className='py-2 flex items-center'>
-        <span className='text-lg font-medium mr-2'>Bitcoin(BTC)</span>
-        <div className='triangle'></div>
-        <span className='percentage ml-auto text-lg font-[600] text-green-500'>4.32%</span>
-      </li>
-      <li className='py-2 flex items-center'>
-        <span className='text-lg font-medium mr-2'>Polygon(MATIC)</span>
-        <div className='triangle'></div>
-        <span className='percentage ml-auto text-lg font-[600] text-green-500'>5.26%</span>
-      </li>
+      {cryptoData.map((crypto) => (
+        <li key={crypto.id} className='py-2 flex items-center'>
+        <img
+              src={crypto.image}
+              alt={crypto.name}
+              style={{ width: '20px', marginRight: '8px' }}
+            />
+          <span className='text-lg font-medium mr-2'>{crypto.name} ({crypto.symbol})</span>
+          <div className='triangle'></div>
+          <span className='percentage text-lg font-[600] ml-auto text-green-500'>
+            {crypto.price_change_percentage_24h.toFixed(2)}%
+          </span>
+        </li>
+      ))}
     </ul>
     </div>
-    </>
+    </div>
   );
 };
 
